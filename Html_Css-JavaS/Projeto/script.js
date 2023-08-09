5d81641ec223b50976fbafa3cfba54ee9fdb73d9
Bruno Broietti <102196468+brunobroiettii@users.noreply.github.com>
2023-08-09 00:38:33 -0300

    Add files via upload
    Adjust container width

--- Html_Css-JavaS/Projeto/script.js
+++ Html_Css-JavaS/Projeto/script.js
@@ -1,11 +1,11 @@
 function toggleMode() {
   const html = document.documentElement
 
-//  if(html.classList.contains("light")) {
-//    html.classList.remove("light")
-//  } else {
-//    html.classList.add("light")f
+// if(body.classlist.contains("light")) {
+//  html.classList.remove("ligh")
+//}  else {
+//   html.classList.add("light")
 //  }
 
   html.classList.toggle("light")
-}
+} 
