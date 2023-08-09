80da96dedc741894699f43867ba5f2f91fa5ce41
Bruno Broietti <102196468+brunobroiettii@users.noreply.github.com>
2023-08-08 22:11:07 -0300

    Add files via upload
    Adicionando códigos

---
+++ Html_Css-JavaS/Projeto/script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement
+
+//  if(html.classList.contains("light")) {
+//    html.classList.remove("light")
+//  } else {
+//    html.classList.add("light")f
+//  }
+
+  html.classList.toggle("light")
+}
